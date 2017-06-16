7000625d88a1c0422b38b351b7a245d1044c76a6
Adam Luzsi <adamluzsi@gmail.com>
2017-06-17 00:06:24 +0200

    format by rubocop

--- payload_manipulator_service/upcase/admin/partial.rb
+++ payload_manipulator_service/upcase/admin/partial.rb
@@ -1,16 +1,12 @@
 require 'rack/app'
 class PayloadManipulatorService::Upcase::Admin::Partial < Rack::App
-
   get '/partial' do
     length = 0
 
-    if params['length']
-      length = params['length'].to_i
-    end
+    length = params['length'].to_i if params['length']
 
     raw_payload = payload.to_s
 
     raw_payload[0...length].to_s.upcase + raw_payload[length..-1].to_s
   end
-
 end
